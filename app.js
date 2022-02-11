2a4bbc0fb166667ea6a34283a3acaf46f6c9bafe
REK7on <tansang.27.10.2003@gmail.com>
2022-02-11 16:04:25 +0700

    fix CORS

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 import express from 'express'
 import morgan from 'morgan'
 import fetch from 'node-fetch'
-
+import cors from 'cors'
 import { validateCheckout } from './middlewares/checkout.js'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+)
 
 // POST route for checkout
 app.post('/api/v1/checkout', validateCheckout, async (req, res) => {
