575afa9aef647c8eef5137f145e5763f8dc187f9
REK7on <tansang.27.10.2003@gmail.com>
2022-02-11 16:00:46 +0700

    add product route

--- middlewares/checkout.js
+++ middlewares/checkout.js
@@ -6,7 +6,7 @@ export const validateCheckout = (req, res, next) => {
     allowUnknown: true,
   })
   if (error) {
-    const message = error.details.map(el => el.message).join(',')
+    const message = error.details.map(el => el.message).join(', ')
     throw new ExpressError(message, 400)
   } else next()
 }
---
+++ routes/products.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.route('/').get()
+router.route('/:id').get()
+
+export default router
